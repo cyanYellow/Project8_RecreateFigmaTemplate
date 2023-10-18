21d3c56126301f024f8786eecb73f9ffd8cc4a10
cyanYellow <williecyanyellow@gmail.com>
2023-10-18 17:44:41 -0600

    Initial Commit

---
+++ Project8_RecreateFigmaTemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project8_RecreateFigmaTemplate
+//
+//  Created by Willie Green on 10/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project8_RecreateFigmaTemplate/Project8_RecreateFigmaTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project8_RecreateFigmaTemplateApp.swift
+//  Project8_RecreateFigmaTemplate
+//
+//  Created by Willie Green on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Project8_RecreateFigmaTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
